2a5e829d4c51b780d3eb8c55c73f3f398ac3f570
varshini <vkeshavostb1@gmail.com>
2019-05-22 11:38:28 +0530

    modified

--- task2/javascript/task2.js
+++ task2/javascript/task2.js
@@ -1,16 +1,19 @@
-var mass1=25;
-var mass2=40;
-var height1=4.7;
-var height2=5.3;
+var mass1=45;
+var mass2=20;
+var height1=5.3;
+var height2=4.7;
 var mark=mass1/height1**2;
 var john=mass2/height2**2;
-var boolean=(mark>john);
+var boolean=mark>john;
 console.log(boolean);
 
-// var boolean;
+// var result= a;
 // if(mark>john){
-// 	console.log(boolean);
+// 	console.log(mark);
 // }
 // else{
-// 	console.log(boolean);
+// 	console.log(john);
 // }
+
+// var markBMIheight = mark > john;
+// console.log(markBMIheight);
